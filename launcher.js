32386ea9d5f0e4bbea61458f02bb6d8b2a8be9bc
vf12776-ux <vf12776-ux@users.noreply.github.com>
2026-05-06 02:08:18 +0300

    Final: working PWA with install and browser redirect

--- launcher.js
+++
@@ -1,5 +0,0 @@
-if (/Android/i.test(navigator.userAgent)) {
-    window.location.href = 'intent://www.yougile.com/board/6ld5e6qu67te#Intent;scheme=https;package=com.android.chrome;end;';
-} else {
-    window.location.href = 'https://www.yougile.com/board/6ld5e6qu67te';
-}
